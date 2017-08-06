2e9fb7e7ba7cc0debb0c6ae86fad78c53e647168
Mark Scannell <markfscannell@gmail.com>
2017-08-05 19:57:52 -0500

    parsed data to json

--- py_finance_website/stockViewer/views.py
+++ py_finance_website/stockViewer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from stockViewer import forms
 from py_finance import YahooFinanceClient
+import json
 
 # Create your views here.
 def index(request):
@@ -11,13 +12,17 @@ def index(request):
         stockSymbolEntryForm = forms.StockSymbolEntryForm(request.POST)
 
         if  stockSymbolEntryForm.is_valid():
+            print('start')
             financeClient = YahooFinanceClient(stockSymbolEntryForm.cleaned_data['stockSymbol'])
+            print('done financeClient')
             stockHistory = financeClient.getHistory(str(stockSymbolEntryForm.cleaned_data['firstDay']),
                                                     str(stockSymbolEntryForm.cleaned_data['secondDay']))
             print("getHistory done")
             stockViewContext['tradingDays'] = stockHistory.tradingDays
+            stockViewContext['dumJson'] = json.dumps([{'a':24}, {'a':32}])
 
     stockViewContext['stockSymbolEntryForm'] = stockSymbolEntryForm
+    stockViewContext['dummy'] = 23
 
     print('return render')
     return render(request,
